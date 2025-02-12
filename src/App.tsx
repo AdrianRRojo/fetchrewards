6cb41efcd8c0f7124c83ffd0bc35fd09dcf2b39d
aricardorojo <aricardorojo@gmail.com>
2025-02-12 22:09:28 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Signup from './views/Signup';
+import Dashboard from './views/Dashboard';
+
+function App() {
+
+
+  return (
+
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+  );
+}
+
+export default App;
---
+++ src/views/Signup.tsx
@@ -0,0 +1,95 @@
+import { useState, type FormEvent } from "react"
+
+
+import "../App.css"
+
+function Signup() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e: FormEvent) => {
+    setLoading(true)
+    e.preventDefault()
+    setMessage("")
+    
+
+
+    try {
+      const response = await fetch("https://frontend-take-home-service.fetch.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email }),
+        credentials: 'include',
+      })
+
+      if (response.ok) {
+        setMessage("Sign up successful!")
+        
+        setTimeout(() => {
+          window.location.href = "/dashboard"
+        }, 2000)
+        
+      } else {
+        setMessage("Sign up failed. Please try again.")
+      }
+    } catch (error) {
+      setMessage("An error occurred. Please try again later.")
+    }
+  }
+
+
+  return (
+
+    <div className="signup-container">
+      <img id="register-logo" src="https://cdn.brandfetch.io/id7Cm60rQf/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B" alt="Fetch Rewards Logo"/>
+      <h2 className="signup-title">Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="submit-button">
+          Sign Up
+        </button>
+      </form>
+      {message && <p className="message">{message}</p>}
+
+      {loading && 
+        <div className="load-container">
+          <div className="load"></div>
+        </div>
+      }
+    </div>
+
+  )
+}
+
+export default Signup
+
